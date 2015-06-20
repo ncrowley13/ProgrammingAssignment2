89b53a86e03b5649bf015b5a252f9fc8760324d0
Nick <wiscrowley@gmail.com>
2015-06-20 11:22:47 -0400

    Coursera Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this main function stores a list of subset functions
+## that act on an input matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<-y
+    m <<-NULL
+  }
+  get <-function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function () m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function calculates the inverse of a matrix
+##or calls it from the cache
+
+cacheSolve <- function(x=solve(), ...) {
+  m<-x$getinverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  solve<-x$get()
+  m<-solve(solve, ...)
+  x$setinverse(m)
+  m
 }
+
